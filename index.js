c96309695f7cd275d90635406139d152566e45db
tangjianfeng <tangjianfeng@exingcai.com>
2017-09-20 09:55:30 +0800

    add: absolute dir

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 const loaderUtils = require('loader-utils')
 
-function HtmlResourceWebpackPlugin(options) {}
+function HtmlResourceWebpackPlugin() {
+    "use strict";
+    this.resourceList = {}
+}
 
 HtmlResourceWebpackPlugin.prototype.apply = function (compiler) {
-
+    const self = this
     function hasProtocal(route) {
         return route.indexOf('http://') === 0 || route.indexOf('https://') === 0 || route.indexOf('//') === 0;
     }
@@ -15,74 +18,93 @@ HtmlResourceWebpackPlugin.prototype.apply = function (compiler) {
         scriptRegex = new RegExp("(<script[^>]*src=([\'\"]*)(.*?)([\'\"]*).*?\>(<\/script>)?)", "ig");
 
     compiler.plugin('compilation', function (compilation) {
-
         compilation.plugin('html-webpack-plugin-before-html-processing', function (htmlPluginData, callback) {
+            const context = compiler.options.context
             const htmlContext = path.dirname(htmlPluginData.plugin.options.template.split('!')[1])
             let content = htmlPluginData.html
 
             const linkMatchResult = content.match(linkRegex) || []
             const scriptMatchResult = content.match(scriptRegex) || []
-
             linkMatchResult.forEach(link=>{
+                let linkDistName = '',
+                    linkFrom = ''
                 const $link = cheerio.load(link),
                     attr = $link('link').attr()
                 if(hasProtocal(attr.href)) return
                 if(attr.__raw || attr.__raw==='') return
-                const linkFrom = path.resolve(htmlContext, attr.href)
-                const data = fs.readFileSync(linkFrom)
-                let outPathAndName = htmlPluginData.plugin.options.resourceName || '[name].[ext]'
-                outPathAndName = outPathAndName.replace('[type]', function () {
-                    return 'css'
-                })
-                const linkName = loaderUtils.interpolateName(
-                    {resourcePath: linkFrom},
-                    outPathAndName,
-                    {content: data})
-                compilation.assets[linkName] = {
-                    size: function () {
-                        return data.length
-                    },
-                    source: function () {
-                        return data
-                    }
+                if(attr.href[0]==='/') {
+                    linkFrom = context + attr.href
+                } else {
+                    linkFrom = path.resolve(htmlContext, attr.href)
                 }
+                if(true || !self[linkFrom]) {
+                    const data = fs.readFileSync(linkFrom)
+                    let outPathAndName = htmlPluginData.plugin.options.resourceName.css || '[name].[ext]'
+
+                    const linkName = loaderUtils.interpolateName(
+                        {resourcePath: linkFrom},
+                        outPathAndName,
+                        {content: data})
+                    compilation.assets[linkName] = {
+                        size: function () {
+                            return data.length
+                        },
+                        source: function () {
+                            return data
+                        }
+                    }
+                    linkDistName = linkName
+                    self[linkFrom] = linkName
+                } else {
+                    linkDistName = self[linkFrom]
+                }
+
                 content = content.replace(link, function () {
-                    return `<link rel="stylesheet" href="${linkName}">`
+                    return `<link rel="stylesheet" href="${linkDistName}">`
                 })
                 htmlPluginData.html = content
             })
 
             scriptMatchResult.forEach(script=>{
+                let scriptDistName = '',
+                    scriptFrom = ''
                 const $script = cheerio.load(script),
                     attr = $script('script').attr()
                 if(hasProtocal(attr.src)) return
                 if(attr.__raw || attr.__raw==='') return
-                const scriptFrom = path.resolve(htmlContext, attr.src)
-                const data = fs.readFileSync(scriptFrom)
-                let outPathAndName = htmlPluginData.plugin.options.resourceName || '[name].[ext]'
-                outPathAndName = outPathAndName.replace('[type]', function () {
-                    return 'style'
-                })
-                const scriptName = loaderUtils.interpolateName(
-                    {resourcePath: scriptFrom},
-                    outPathAndName,
-                    {content: data})
+                if(attr.href[0]==='/') {
+                    scriptFrom = context + attr.href
+                } else {
+                    scriptFrom = path.resolve(htmlContext, attr.href)
+                }
+                if(true || !self[scriptFrom]){
+                    const data = fs.readFileSync(scriptFrom)
+                    let outPathAndName = htmlPluginData.plugin.options.resourceName.js || '[name].[ext]'
 
-                compilation.assets[scriptName] = {
-                    size: function () {
-                        return data.length
-                    },
-                    source: function () {
-                        return data
+                    const scriptName = loaderUtils.interpolateName(
+                        {resourcePath: scriptFrom},
+                        outPathAndName,
+                        {content: data})
+
+                    compilation.assets[scriptName] = {
+                        size: function () {
+                            return data.length
+                        },
+                        source: function () {
+                            return data
+                        }
                     }
+                    scriptDistName = scriptName
+                    self[scriptFrom] = scriptName
+                } else {
+                    scriptDistName = self[scriptFrom]
                 }
 
                 content = content.replace(script, function () {
-                    return `<script src="${scriptName}"></script>`
+                    return `<script src="${scriptDistName}"></script>`
                 })
                 htmlPluginData.html = content
             })
-
             callback(null, htmlPluginData)
         })
     })
